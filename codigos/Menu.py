8c981c1c993e02c1a6750e4ce7ca539ee9020612
Michael Marlon <Email.com>
2026-03-18 20:52:48 -0300

    Criadas classes Menu e Jogo

---
+++ codigos/Menu.py
@@ -0,0 +1,6 @@
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        pass
+    def run(self):
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import pygame
-pygame.init()
+from codigos.Jogo import Jogo
 
-windows = pygame.display.set_mode(size=(1000,800))
-while True:
-    # checando todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()# fechando a janela
-            quit()# finalizando pygame
+jogo = Jogo()
+jogo.run()
